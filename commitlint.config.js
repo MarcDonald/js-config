9938f7b666c79c5490dc2dca4427aa5fe2c08261
Marc Donald <dev@marcdonald.com>
2023-03-08 22:54:54 +0000

    build(root): add root scope to commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,35 @@
+const {getPackages} = require('@commitlint/config-lerna-scopes').utils;
+
 module.exports = {
 	extends: [
 		'@commitlint/config-conventional',
 		'@commitlint/config-lerna-scopes',
 	],
+	prompt: {
+		questions: {
+			breakingBody: {
+			},
+		}
+	},
+	rules: {
+		'scope-enum': (ctx) =>
+				getPackages(ctx).then((packages) => [2, 'always', [...packages, 'root']]),
+
+		'type-enum': () => [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'style',
+				'refactor',
+				'test',
+				'revert',
+			],
+		],
+	},
 };
