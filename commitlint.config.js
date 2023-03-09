40632a3ca24a919251fcb6e96990cb72659f581d
Marc Donald <dev@marcdonald.com>
2023-03-09 23:51:45 +0000

    feat(commitlint-config): add commitlint-config package

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,34 +2,11 @@ const {getPackages} = require('@commitlint/config-lerna-scopes').utils;
 
 module.exports = {
 	extends: [
-		'@commitlint/config-conventional',
+		'@marcdonald/commitlint-config',
 		'@commitlint/config-lerna-scopes',
 	],
-	prompt: {
-		questions: {
-			breakingBody: {
-			},
-		}
-	},
 	rules: {
 		'scope-enum': (ctx) =>
 				getPackages(ctx).then((packages) => [2, 'always', [...packages, 'root']]),
-
-		'type-enum': () => [
-			2,
-			'always',
-			[
-				'feat',
-				'fix',
-				'build',
-				'chore',
-				'ci',
-				'docs',
-				'style',
-				'refactor',
-				'test',
-				'revert',
-			],
-		],
 	},
 };
---
+++ packages/commitlint-config/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+	extends: [
+		'@commitlint/config-conventional',
+	],
+	prompt: {
+		questions: {
+			breakingBody: {
+			},
+		}
+	},
+
+	rules: {
+		'type-enum': () => [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'style',
+				'refactor',
+				'test',
+				'revert',
+			],
+		],
+	},
+};
