e6030ddc71df1380a4edad2894b22a81a39f0bb4
Marc Donald <dev@marcdonald.com>
2023-03-26 20:28:43 +0100

    fix(eslint-config): disable no-unused-vars when variable starts with an underscore

--- packages/eslint-config/rules/core-typescript.js
+++ packages/eslint-config/rules/core-typescript.js
@@ -1,7 +1,15 @@
 module.exports = {
-	'@typescript-eslint/no-unused-vars': ['error'],
 	'@typescript-eslint/explicit-function-return-type': ['off'],
 	'@typescript-eslint/explicit-module-boundary-types': ['off'],
 	'@typescript-eslint/no-empty-function': ['off'],
 	'@typescript-eslint/no-explicit-any': ['off'],
+	'no-unused-vars': 'off',
+	'@typescript-eslint/no-unused-vars': [
+		'error',
+		{
+			argsIgnorePattern: '^_',
+			varsIgnorePattern: '^_',
+			caughtErrorsIgnorePattern: '^_',
+		},
+	],
 };
